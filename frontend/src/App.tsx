d2029db6c95934f9ce9539a070a2ef3acd711e7b
Kelvin <chuakid@gmail.com>
2023-08-31 19:04:21 +0800

    header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import { AppShell } from "@mantine/core";
+import { AppShell, Header, Text } from "@mantine/core";
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <AppShell p="lg">
+    <AppShell
+      p="lg"
+      header={
+        <Header p="xs" height={60}>
+          <Text size={24} component={Link} to="/">Home</Text>
+        </Header>
+      }
+    >
       <Outlet></Outlet>
     </AppShell>
   );
