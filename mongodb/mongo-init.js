7790a6f18e0cc7f9cbd8adfe1222872ecd17b3e2
Bacon-Strips <86605835+Bacon-Strips@users.noreply.github.com>
2023-09-06 16:17:20 +0800

    feat: add more placeholder questions to database

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -49,4 +49,194 @@ db.questions.insertMany([
     categories: ["Data Structures", "Algorithms"],
     complexity: "Easy",
   },
+  {
+    title: "Two Sum",
+    description: `Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+
+    Example 1:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+    Example 2:
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+
+    Example 3:
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
+    
+    Constraints:
+
+    2 <= nums.length <= 104
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+    Only one valid answer exists.`,
+    categories: ["Algorithms", "Searching"],
+    complexity: "Easy",
+  },
+  {
+    title: "Longest Substring Without Repeating Characters",
+    description: `Given a string s, find the length of the longest substring without repeating characters.
+    
+    Example 1:
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+
+    Example 2:
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+    Example 3:
+    Input: s = "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+    
+    Constraints:
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.`,
+    categories: ["Algorithms"],
+    complexity: "Medium",
+  },
+  {
+    title: "Median of Two Sorted Arrays",
+    description: `Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+    The overall run time complexity should be O(log (m+n)).
+    
+    Example 1:
+    Input: nums1 = [1,3], nums2 = [2]
+    Output: 2.00000
+    Explanation: merged array = [1,2,3] and median is 2.
+
+    Example 2:
+    Input: nums1 = [1,2], nums2 = [3,4]
+    Output: 2.50000
+    Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+    
+    Constraints:
+    nums1.length == m
+    nums2.length == n
+    0 <= m <= 1000
+    0 <= n <= 1000
+    1 <= m + n <= 2000
+    -106 <= nums1[i], nums2[i] <= 106`,
+    categories: ["Algorithms", "Searching"],
+    complexity: "Hard",
+  },
+  {
+    title: "Longest Palindromic Substring",
+    description: `Given a string s, return the longest palindromic substring in s.
+    
+    Example 1:
+    Input: s = "babad"
+    Output: "bab"
+    Explanation: "aba" is also a valid answer.
+
+    Example 2:
+    Input: s = "cbbd"
+    Output: "bb"
+    
+    Constraints:
+    1 <= s.length <= 1000
+    s consist of only digits and English letters.`,
+    categories: ["Algorithms"],
+    complexity: "Medium",
+  },
+  {
+    title: "Longest Substring Without Repeating Characters",
+    description: `Given a string s, find the length of the longest substring without repeating characters.
+    
+    Example 1:
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+
+    Example 2:
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+    Example 3:
+    Input: s = "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+     
+    Constraints:
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.`,
+    categories: ["Algorithms", "Dynamic Programming"],
+    complexity: "Medium",
+  },
+  {
+    title: "Merge k Sorted Lists",
+    description: `You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+    Merge all the linked-lists into one sorted linked-list and return it.
+    
+    Example 1:
+    Input: lists = [[1,4,5],[1,3,4],[2,6]]
+    Output: [1,1,2,3,4,4,5,6]
+    Explanation: The linked-lists are:
+    [
+      1->4->5,
+      1->3->4,
+      2->6
+    ]
+    merging them into one sorted list:
+    1->1->2->3->4->4->5->6
+
+    Example 2:
+    Input: lists = []
+    Output: []
+
+    Example 3:
+    Input: lists = [[]]
+    Output: []
+     
+    Constraints:
+    k == lists.length
+    0 <= k <= 104
+    0 <= lists[i].length <= 500
+    -104 <= lists[i][j] <= 104
+    lists[i] is sorted in ascending order.
+    The sum of lists[i].length will not exceed 104.`,
+    categories: ["Algorithms, Searching", "Sorting"],
+    complexity: "Hard",
+  },
+  {
+    title: "Regular Expression Matching",
+    description: `Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+    '.' Matches any single character.​​​​
+    '*' Matches zero or more of the preceding element.
+    The matching should cover the entire input string (not partial).
+    
+    Example 1:
+    Input: s = "aa", p = "a"
+    Output: false
+    Explanation: "a" does not match the entire string "aa".
+
+    Example 2:
+    Input: s = "aa", p = "a*"
+    Output: true
+    Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+    Example 3:
+    Input: s = "ab", p = ".*"
+    Output: true
+    Explanation: ".*" means "zero or more (*) of any character (.)".
+     
+    Constraints:
+    1 <= s.length <= 20
+    1 <= p.length <= 20
+    s contains only lowercase English letters.
+    p contains only lowercase English letters, '.', and '*'.
+    It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.`,
+    categories: ["Algorithms", "Data Structure"],
+    complexity: "Hard",
+  },
 ]);
