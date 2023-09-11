94eee4a5868dc9b1343dfe8f808f71b3415bb79f
Shao Wee <lee_shao_wee@hotmail.com>
2023-09-11 09:26:03 +0800

    fix: update default access_type to 3

--- user-service/controllers/userController.go
+++ user-service/controllers/userController.go
@@ -66,7 +66,7 @@ func Register(c *fiber.Ctx) error {
 	access_type, parseErr := utils.ParseUint(data["access_type"])
 
 	if parseErr != nil || authErr != nil || token != SecretKey {
-		access_type = 2
+		access_type = 3
 	}
 
 	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
