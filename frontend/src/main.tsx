ee05120faf266e05168389e9ebcd625ff2447028
Kelvin <chuakid@gmail.com>
2023-09-15 16:09:51 +0800

    feat: admin protected routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import QuestionPage from "./pages/QuestionPage.tsx";
@@ -15,6 +10,7 @@ import CreateQuestionPage from "./pages/CreateQuestionPage.tsx";
 import UserContextProvider from "./contexts/UserContext.tsx";
 import ProtectedRoute from "./routing/ProtectedRoute.tsx";
 import LoginPage from "./pages/LoginRegisterPage.tsx";
+import AdminRoute from "./routing/AdminRoute.tsx";
 
 const queryClient = new QueryClient();
 
@@ -32,9 +28,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
               <Route path="/login" element={<LoginPage />} />
               <Route element={<ProtectedRoute />}>
                 <Route element={<App />}>
+                  <Route element={<AdminRoute />}>
+                    <Route path="/create" element={<CreateQuestionPage />} />
+                  </Route>
                   <Route path="/" index element={<LandingPage />} />
                   <Route path="/question/:id" element={<QuestionPage />} />
-                  <Route path="/create" element={<CreateQuestionPage />} />
 
                   <Route path="*" element={<Navigate to="/" replace />} />
                 </Route>
