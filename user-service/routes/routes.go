83844f8d6c387e6428e57100db0b985ef31ceca3
Shao Wee <lee_shao_wee@hotmail.com>
2023-09-17 11:23:17 +0800

    fix: updated package name, controller name - renamed backend to user-service - UserJwt -> GetUserByJwt - moved SecretKey to .env file

--- user-service/routes/routes.go
+++ user-service/routes/routes.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"backend/controllers"
+	"user-service/controllers"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,7 +11,7 @@ func Setup(app *fiber.App) {
 
 	user_api := api.Group("/user")
 	user_api.Get("", controllers.GetUserById)
-	user_api.Post("", controllers.UserJwt)
+	user_api.Post("", controllers.GetUserByJwt)
 	user_api.Get("/all", controllers.GetAllUsers)
 	user_api.Post("/register", controllers.Register)
 	user_api.Post("/deregister", controllers.Deregister)
