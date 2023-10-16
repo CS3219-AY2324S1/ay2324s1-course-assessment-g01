ba38965290a08be85135357147471aae8603b22a
Kelvin <chuakid@gmail.com>
2023-10-16 20:07:14 +0800

    feat: chat animations

--- frontend/src/components/ChatComponent.tsx
+++ frontend/src/components/ChatComponent.tsx
@@ -1,5 +1,6 @@
 import { Badge, Input, Stack, Title } from "@mantine/core";
-import { useEffect, useState } from "react";
+import styles from "./ChatComponent.module.css";
+import { useEffect, useRef, useState } from "react";
 import * as Y from "yjs";
 
 export type ChatMessage = {
@@ -15,18 +16,31 @@ const ChatComponent = ({
 }) => {
   const [chatlogState, setchatlogState] = useState<ChatMessage[]>([]);
   const [currentMessage, setCurrentMessage] = useState<string>("");
+  const bottomRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const set = () => setchatlogState(chatlog.toArray());
     chatlog.observe(set);
     return () => chatlog.unobserve(set);
   }, [chatlog]);
 
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView();
+  }, [chatlogState]);
+
   return (
     <Stack justify="space-between" style={{ flex: 1, minHeight: 0 }}>
       <Title size={"h2"}>Chat</Title>
-      <Stack style={{ overflow: "auto", flex: 1, minHeight: 0 }}>
+      <Stack
+        style={{
+          overflowY: "auto",
+          overflowX: "hidden",
+          flex: 1,
+          minHeight: 0,
+        }}
+      >
         {chatlogState.map((message) => (
           <Badge
+            className={styles[id === message.userId ? "slideRight" : "slideLeft"]}
             mih={24}
             style={{
               alignSelf: id === message.userId ? "flex-end" : "flex-start",
@@ -36,6 +50,7 @@ const ChatComponent = ({
             {message.message}
           </Badge>
         ))}
+        <div ref={bottomRef}></div>
       </Stack>
       <div>
         <Input
