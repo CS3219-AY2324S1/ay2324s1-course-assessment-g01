3a392effcdd0ed341a54f51d382501d633c2a6d5
Shao Wee <lee_shao_wee@hotmail.com>
2023-10-19 21:30:25 +0800

    fix: updated question_id type in model

--- collaboration-service/models/room_model.go
+++ collaboration-service/models/room_model.go
@@ -1,9 +1,9 @@
 package models
 
 type Room struct {
-	RoomId     uint `json:"room_id" gorm:"primaryKey;gorm:autoIncrement"`
-	QuestionId uint `json:"question_id" gorm:"not null"`
-	UserAId    uint `json:"user_a_id" gorm:"not null"`
-	UserBId    uint `json:"user_b_id" gorm:"not null"`
-	IsOpen     bool `json:"is_open" gorm:"default:true"`
+	RoomId     uint   `json:"room_id" gorm:"primaryKey;gorm:autoIncrement"`
+	QuestionId string `json:"question_id" gorm:"not null"`
+	UserAId    uint   `json:"user_a_id" gorm:"not null"`
+	UserBId    uint   `json:"user_b_id" gorm:"not null"`
+	IsOpen     bool   `json:"is_open" gorm:"default:true"`
 }
